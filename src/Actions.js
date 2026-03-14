72a039410806f5f49e9fcdb6a04bf4a0d6ed3e67
Raymond Oyondi <raymondoyondi@gmail.com>
2026-03-14 12:07:10 -0500

    Create Actions.js

---
+++ src/Actions.js
@@ -0,0 +1,14 @@
+const ACTIONS = {
+    JOIN: 'join',
+    JOINED: 'joined',
+    DISCONNECTED: 'disconnected',
+    CODE_CHANGE: 'code-change',
+    SYNC_CODE: 'sync-code',
+    LEAVE: 'leave',
+    SEND_MESSAGE: 'send-message',
+    RECEIVE_MESSAGE: 'receive-message',
+    TOGGLE_EDITOR_LOCK: 'TOGGLE_EDITOR_LOCK',
+    USER_CHANGES: 'user-changes',
+};
+
+module.exports = ACTIONS;
